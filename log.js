f1c8e81de9d799fcaadec1dd1c0cae71547090dc
James Graham <james10graham@gmail.com>
2016-10-15 14:01:10 +0100

    add url check for codeship

--- log.js
+++ log.js
@@ -1,5 +1,9 @@
 const app = require('express')();
 
+app.get('/', function (req, res) {
+    res.send('Hello Codeship!');
+});
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("Yay! We have succeeded");
 });
